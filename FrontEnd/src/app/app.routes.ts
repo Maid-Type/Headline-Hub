65d8f02cc7f2a153201cee83fbb4f3ab2431763d
Maid Mešić <maidmesic0503@gmail.com>
2025-01-16 16:47:46 +0100

    Update app.routes.ts

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 import {NewsComponent} from './news/news.component';
 import {AdminDashboardComponent} from './admindashboard/admindashboard.component';
-import {UserdashboardComponent} from './userdashboard/userdashboard.component';
 import {ChangePasswordComponent} from './change-password/change-password.component';
 import {HomeComponent} from './home/home.component';
 import {SpecificnewsComponent} from './specificnews/specificnews.component';
+import {CreateNewsComponent} from './create-news/create-news.component';
+import {EditNewsComponent} from './edit-news/edit-news.component';
 
 export const routes: Routes = [
   {path: 'home', component: HomeComponent},
@@ -14,8 +15,9 @@ export const routes: Routes = [
   {path: 'register', component: RegisterComponent},
   {path: 'news', component: NewsComponent},
   {path: 'news/:id', component: SpecificnewsComponent},
+  {path: 'createnews', component: CreateNewsComponent},
+  {path: 'editnews/:id', component: EditNewsComponent},
   {path: 'admindashboard', component: AdminDashboardComponent},
-  {path: 'userdashboard', component: UserdashboardComponent},
   {path: 'change-password', component: ChangePasswordComponent},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
